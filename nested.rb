5367566e35ee0b822efbafd6304655b24ff50000
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-12 17:39:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info.to_a
+		 alans_new_info = Array.new
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 		#  binding.pry
 
 
