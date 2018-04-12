60bc3333e6d98549f468d629d0f9d9711a5ff9f3
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-12 17:16:07 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages].to_s.chomp
+programmer_hash[:dennis_ritchie][:languages].join()
 end
 
 def adding_matz
